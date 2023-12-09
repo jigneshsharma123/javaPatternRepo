e0af03f28f1f454d4ad6c44c92774e7aad9f0f37
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-09 09:43:57 +0530

    setting up the java for printing the rightAngleTriangle

---
+++ RightTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class RightTriangle {
+    
+    
+    public static void PrintRightTriangle(int n) {
+         System.out.println("you inputed the number!!");
+    }
+   
+
+    
+    public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the size of the pattern: ");
+     int n = sc.nextInt();
+    PrintRightTriangle(n);
+    }
+}
