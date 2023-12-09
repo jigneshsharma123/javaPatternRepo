d82268664073b2e579ae9cc98d822b1ceff663f9
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-09 11:13:36 +0530

    program of the InvertedRightTriangle is done.

--- RightTriangle.java
+++ RightTriangle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class RightTriangle {
     
-    
+    //Right Triangle Pattern
     public static void PrintRightTriangle(int n) {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
@@ -15,14 +15,34 @@ class RightTriangle {
             System.err.println();
         }
     }
+    // Inverted Right Triangle pattern
+    public static void InvertedRightTriangle(int n ) {
+     for(int i = n; i>=1; i--) {
+        for(int j = 1; j <= n; j++) {
+            if(i>=j) {
+                System.out.print("*");
+            } else {
+                System.err.print(" ");
+            }
+        }
+        System.out.println();
+     }
+   
+    }
    
 
     
     public static void main(String[] args) {
 
-     Scanner sc = new Scanner(System.in);
-     System.out.println("Enter the size of the pattern: ");
-     int n = sc.nextInt();
-    PrintRightTriangle(n);
+    try {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the size of the pattern: ");
+         int n = sc.nextInt();
+        InvertedRightTriangle(n);
+
+        } catch (Exception e) {
+        System.out.println(e);
+    }
+    // PrintRightTriangle(n);
     }
 }
