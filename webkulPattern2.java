e98b8ddc13fc1d84eb34a057450212ce05363b34
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-14 19:06:08 +0530

    finally completed the pattern

--- webkulPattern2.java
+++ webkulPattern2.java
@@ -12,6 +12,42 @@ public class webkulPattern2 {
             }
          System.out.println();
         } //end of the pyramid 
-
+      for(int i = 0; i < n; i++) {
+          for(int j = 0; j < (n/2)+1; j++) {
+            if(j>=n/2-i && j>= i - n / 2) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+          } //end of the left part of the patten
+          //now it's time to print the @ symbol
+          for(int j = 0; j < n; j++) {
+            if( i == n / 2) {
+                System.out.print("@");
+            } else {
+                System.out.print(" ");
+            }
+          }
+          for(int j=0; j< (n/2) + 1; j++) {
+            if( j >= n / 2 - i && j >= i - n / 2) {
+                System.out.print("*");
+            }
+          }
+          System.out.println();
+      }
     }
 }
+/*
+ * 
+ *   @
+    @@@
+   @@@@@
+  @@@@@@@
+  *     *
+ **     **
+***@@@@@***
+ **     **
+  *     *
+
+ * 
+ */
