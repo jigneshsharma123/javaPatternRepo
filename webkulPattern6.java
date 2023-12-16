8447ce4fa7eed765e8295dfe3f8fc57bd1f2d752
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-16 15:06:32 +0530

    webkul pattern 6 completed

--- webkulPattern6.java
+++ webkulPattern6.java
@@ -1,7 +1,70 @@
 public class webkulPattern6 {
     public static void main(String[] args) {
-    
-         System.out.printf("'hello world'");
+        int n = 5;
 
+        // First part of the pattern
+        for (int i = 0; i <= n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n / 2 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Second part of the pattern
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n; j++) {
+               
+                    if (j == 0 || j == n - 1) {
+                        System.out.print("@");
+                    } else {
+                        System.out.print(" ");
+                    }
+                
+            }
+            System.out.println();
+        }
+
+        // Third part of the pattern
+        for (int i = 0; i <= n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < n - (2 * i); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n / 2; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && (j == 0 || j == n - 1)) {
+                    System.out.print("@");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < n / 2; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j < n - (2 * i); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
+
