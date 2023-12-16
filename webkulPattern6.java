3b7aa794dd0f9c9dcd03eb81b500ffd88ba73782
jignesh sharma <jigneshsharma9868@gmail.com>
2023-12-16 17:23:52 +0530

    made some improvement in the pattern 6

--- webkulPattern6.java
+++ webkulPattern6.java
@@ -3,13 +3,15 @@ public class webkulPattern6 {
         int n = 5;
 
         // First part of the pattern
-        for (int i = 0; i <= n / 2; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n / 2 + 1; i++) {
+           
+            for(int space = 0; space < n/2; space++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < n / 2 - i; j++) {
+            for(int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
+          
             for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
